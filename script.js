ea9df97987179356ca385ae1ba8a29d06e4fe67e
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-03-30 11:43:16 +0200

    pas colors

--- script.js
+++ script.js
@@ -125,33 +125,33 @@
 				}
 			})
 		},
-		colorinit:function(){
-			var colors = ["DF06FF", "50FFFA", "FFFC00", "FFD300", "2116FF"];
-			function randcol (arg) {
-				if(!prevrand){
-					var prevrand = arg.length+1
-				}
-				var  rand = arg[Math.floor((Math.random()*arg.length))];
-				while (rand === prevrand) {
-					var rand = arg[Math.floor((Math.random()*arg.length))];
-				}
-				prevrand = rand;
-				return rand;
-			};
-			var inter = setInterval(function(){
-				$('a').each(function(){
-					$(this).css("color", '#'+randcol(colors));
-				});
-				$('#linkfavicon').attr("href", randcol(colors)+'.ico');
+		// colorinit:function(){
+		// 	var colors = ["DF06FF", "50FFFA", "FFFC00", "FFD300", "2116FF"];
+		// 	function randcol (arg) {
+		// 		if(!prevrand){
+		// 			var prevrand = arg.length+1
+		// 		}
+		// 		var  rand = arg[Math.floor((Math.random()*arg.length))];
+		// 		while (rand === prevrand) {
+		// 			var rand = arg[Math.floor((Math.random()*arg.length))];
+		// 		}
+		// 		prevrand = rand;
+		// 		return rand;
+		// 	};
+		// 	var inter = setInterval(function(){
+		// 		$('a').each(function(){
+		// 			$(this).css("color", '#'+randcol(colors));
+		// 		});
+		// 		$('#linkfavicon').attr("href", randcol(colors)+'.ico');
 
-				$('.colors').each(function(){
-					$(this).css("color", '#'+randcol(colors));
-				});
-				$('.bgcolors').each(function(){
-					$(this).css("background-color", '#'+randcol(colors));
-				});
-			}, 100);
-		},
+		// 		$('.colors').each(function(){
+		// 			$(this).css("color", '#'+randcol(colors));
+		// 		});
+		// 		$('.bgcolors').each(function(){
+		// 			$(this).css("background-color", '#'+randcol(colors));
+		// 		});
+		// 	}, 100);
+		// },
 		connectstatus:function(){
 			if(!localStorage.consta || localStorage.getItem("consta") != "Y88T*)m.y%8E=%By+{.6@VsoR5{883R0+oi;c&UA81Z2Y,<9d_NFdwyjl]Qx)EmxM(0Xym,VM@aH9L.*wU-5-p%/8Eb5357aG67G[DQG./oX=@eE28!IkqZ0q4vzQ^Y#rSfo02vs=6*0<[5!fQ2>;5s3J61V5mUKv1PeX[a0kCIB)1o~N5DL6n{E0_U@3MmG258@MT/z@$8{nq&^Dd@7=E8A<1vwmsfYt7880#eF5>2ybK1L09~oq7b-O0!X4dIf"){
 				$('#connectstatus').html("<a href='inscription.html' id='lienincrip'>Inscription</a><br><a href='connection.html' id='lienconnec'>Conection</a>");
